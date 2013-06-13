799e598912be04a3766ab78439cc7b5e001f0b9e
Andy McKay <amckay@mozilla.com>
2013-06-13 15:14:34 -0700

    fix port

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from collections import defaultdict
-import json
+import os
+
 from flask import Flask, render_template, request
 import requests
 
@@ -51,11 +51,11 @@ def tiers():
         result = fill_tiers(res.json())
         return render_template('tiers.html', result=result['objects'],
                                regions=regions, sorted=regions_sorted,
-                               methods=methods)
+                               methods=methods, server=request.form['server'])
 
     return render_template('tiers.html')
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
