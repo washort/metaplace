ed7c4cae93ccba89341d1099d3ab49885152ebdb
Wil Clouser <clouserw@gmail.com>
2014-01-03 13:55:07 -0800

    add URLs as a title attribute

--- app.py
+++ app.py
@@ -85,15 +85,15 @@ def apikiosk():
                                  local.PINGDOM_ACCOUNT_EMAIL);
     results = []
 
-    checks = cache.get('apikiosk-checks')
+    checks = cache.get('apikiosk.checks')
     if not checks:
         checks = pingdom.getChecks()
-        cache.set('apikiosk-checks', checks, timeout=60 * 60)
+        cache.set('apikiosk.checks', checks, timeout=60 * 60)
 
     for check in checks:
         if check.name.startswith("MKT -"):
 
-            c = cache.get('apikiosk-%s' % check.id)
+            c = cache.get('apikiosk.%s' % check.id)
             if not c:
                 # This takes the average response time from the past 24 hours
                 # pingdomlib uses a reserved keyword (from) as a kwarg :(
@@ -102,7 +102,12 @@ def apikiosk():
 
                 c = {'avg': avg, 'id': check.id, 'name': check.name,
                      'status': check.status}
-                cache.set('apikiosk-%s' % check.id, c, timeout=60 * 60)
+
+                d = check.getDetails()
+
+                c['url'] = "%s%s" % (check.hostname, d['type']['http']['url'])
+
+                cache.set('apikiosk.%s' % check.id, c, timeout=60 * 60)
 
             results.append(c)
 
