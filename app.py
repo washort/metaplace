efb0884d297c231f7a0df7e979b32279df1a67be
Wil Clouser <clouserw@gmail.com>
2014-04-08 10:29:09 -0700

    update apikiosk to show source/CDN differences

--- app.py
+++ app.py
@@ -84,35 +84,63 @@ def apikiosk():
                                  local.PINGDOM_PASS,
                                  local.PINGDOM_APIKEY,
                                  local.PINGDOM_ACCOUNT_EMAIL);
-    results = []
 
     checks = cache.get('apikiosk.checks')
     if not checks:
         checks = pingdom.getChecks()
         cache.set('apikiosk.checks', checks, timeout=60 * 60)
 
-    for check in checks:
-        if check.name.startswith("MKT -"):
+    fireplace = cache.get('apikiosk.fireplace')
+    other = cache.get('apikiosk.other')
 
-            c = cache.get('apikiosk.%s' % check.id)
-            if not c:
-                # This takes the average response time from the past 24 hours
+    if not (fireplace and other):
+        fireplace = []
+        other = []
+        results = {}
+        for check in checks:
+            if check.name.startswith("MKT;"):
+                # This takes the average response time from the past 24hr
                 # pingdomlib uses a reserved keyword (from) as a kwarg :(
                 avg = check.averages(**{'from': int(time.time())-86400})
                 avg = avg['responsetime']['avgresponse']
 
-                c = {'avg': avg, 'id': check.id, 'name': check.name[5:],
-                     'status': check.status}
+                name = check.name[5:].replace('; CDN','')
+
+                if name in results:
+                    c = results[name]
+                else:
+                    c = {'area': "",
+                         'cdn_avg': "",
+                         'cdn_url': "",
+                         'name': name,
+                         'source_avg': "",
+                         'source_url': ""}
 
                 d = check.getDetails()
+                if check.name.endswith('CDN'):
+                    c['cdn_avg'] = avg
+                    c['cdn_url'] = "%s%s" % (check.hostname,
+                                             d['type']['http']['url'])
+                else:
+                    c['source_avg'] = avg
+                    c['source_url'] = "%s%s" % (check.hostname,
+                                                d['type']['http']['url'])
 
-                c['url'] = "%s%s" % (check.hostname, d['type']['http']['url'])
+                if 'Fireplace' in check.name:
+                    c['area'] = 'fireplace'
 
-                cache.set('apikiosk.%s' % check.id, c, timeout=60 * 60)
+                results[name] = c
 
-            results.append(c)
+        for j, k in results.iteritems():
+            if k['area'] == 'fireplace':
+                fireplace.append(k)
+            else:
+                other.append(k)
 
-    return render_template('apikiosk.html', checks=results)
+    cache.set('apikiosk.fireplace', fireplace, timeout=60 * 60)
+    cache.set('apikiosk.other', other, timeout=60 * 60)
+
+    return render_template('apikiosk.html', fireplace=fireplace, other=other)
 
 
 def notify(msg, *args):
